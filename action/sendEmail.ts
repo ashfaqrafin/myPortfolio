cf3f949840b6dfd23b7c339250708e5b0a3b874f
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-09-08 10:18:31 +0600

    complete the contact section

---
+++ action/sendEmail.ts
@@ -0,0 +1,77 @@
+// "use server";
+// import { Resend } from "resend";
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// import { validateString } from "@/lib/utils";
+// import { getErrorMessage } from "@/lib/utils";
+
+// export const sendEmail = async (formData: FormData) => {
+//   const senderEmail = formData.get("senderEmail");
+//   const message = formData.get("message");
+
+//   if (!validateString(senderEmail, 500)) {
+//     return {
+//       error: "Invalid Email",
+//     };
+//   }
+//   if (!validateString(message, 5000)) {
+//     return {
+//       error: "Invalid Message",
+//     };
+//   }
+
+//   try {
+//     await resend.emails.send({
+//       from: "Porfolio Contact<onboarding@resend.dev>",
+//       to: "ashfaqrafin12345@gmail.com",
+//       subject: "Welcome to Resend",
+//       reply_to: senderEmail as string,
+//       text: message as string,
+//     });
+//   } catch (error: unknown) {
+//     return {
+//       error: getErrorMessage(error),
+//     };
+//   }
+// };
+"use server";
+
+import React from "react";
+import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (formData: FormData) => {
+  const senderEmail = formData.get("senderEmail");
+  const message = formData.get("message");
+
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
+    };
+  }
+
+  try {
+    await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
+      to: "ashfaqrafin12345@gmail.com",
+      subject: "Message from contact form",
+      reply_to: senderEmail as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
+};
--- components/contact.tsx
+++ components/contact.tsx
@@ -3,6 +3,8 @@ import { FaPaperPlane } from "react-icons/fa";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/action/sendEmail";
+import SubmitBtn from "./submit-btn";
 const Contact = () => {
   const { ref } = useSectionInView("Contact");
   return (
@@ -34,7 +36,7 @@ const Contact = () => {
       <form
         className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
-          console.log(formData);
+          await sendEmail(formData);
         }}
       >
         <input
@@ -52,13 +54,7 @@ const Contact = () => {
           required
           maxLength={5000}
         />
-        <button
-          type="submit"
-          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 dark:bg-white dark:bg-opacity-10 disabled:scale-100 disabled:bg-opacity-65"
-        >
-          Send
-          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1" />
-        </button>
+        <SubmitBtn />
       </form>
     </motion.section>
   );
