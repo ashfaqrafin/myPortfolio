bf51c2f85eeb48c4c46f5c32cf26f2bbb633efac
Ashfaq Khan Rafin <yourgitemail@example.com>
2023-09-29 13:20:31 +0600

    nothing

--- lib/data.ts
+++ lib/data.ts
@@ -61,13 +61,13 @@ export const experiencesData = [
 ] as const;
 
 export const projectsData = [
-  {
-    title: "CorpComment",
-    description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
-  },
+  // {
+  //   title: "CorpComment",
+  //   description:
+  //     "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+  //   tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+  //   imageUrl: corpcommentImg,
+  // },
   {
     title: "rmtDev",
     description:
