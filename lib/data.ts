b82939ef55c9b5bc147fed65695c561cb1fb3e77
Ashfaq Khan <82702762+ashfaqrafin@users.noreply.github.com>
2024-01-13 19:01:11 +0600

    Add pending project image

--- lib/data.ts
+++ lib/data.ts
@@ -4,6 +4,7 @@ import { FaReact, FaUniversity } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
+import pendingImg from "@/public/pending.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
 
 export const links = [
@@ -68,19 +69,24 @@ export const projectsData = [
   //   tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
   //   imageUrl: corpcommentImg,
   // },
+  // {
+  //   title: "rmtDev",
+  //   description:
+  //     "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+  //   tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+  //   imageUrl: rmtdevImg,
+  // },
+  // {
+  //   title: "Word Analytics",
+  //   description:
+  //     "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+  //   tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+  //   imageUrl: wordanalyticsImg,
+  // },
   {
-    title: "rmtDev",
-    description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+    title: "Pending",
+    description: "working on my Projects",
+    imageUrl: pendingImg,
   },
 ] as const;
 
